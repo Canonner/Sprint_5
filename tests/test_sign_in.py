da27fcbd7a2537567477b718348ea9b5ab2edbf3
Canonner <canonner@gmail.com>
2023-10-08 03:59:12 +0300

    1. Добавлен файл data.py с тестовыми данными в папку tests 2. Фикстура генерации данных для регистрации перенесена из файла conftest.py в файл data.py как статический метод. 3. В файле locators.py локаторы разделены на классы, соответствующие страницам сайта. 4. Во всех тестовых файлах изменены вызовы локаторов в соответствии с из обновленными классами, а также вызовы тестовых данных 5. Добавлено описание файла data.py в README.md, скорректировано описание фикстур 6. Комментарии исправлены на docstring

--- tests/test_sign_in.py
+++ tests/test_sign_in.py
@@ -1,46 +1,47 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from locators import Locators
+from locators import MainPageLocators, AuthPageLocators, RegisterPageLocators, ForgotPasswordPageLocators
+from data import CommonData
 
 
 class TestSignIn:
 
-    # проверка входа по кнопке «Войти в аккаунт» на главной
     def test_authorization_via_sign_in_account_button(self, driver):
-        driver.find_element(*Locators.SIGN_IN_ACCOUNT_BUTTON_MAIN).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.EMAIL_INPUT_FIELD_AUTH))
-        driver.find_element(*Locators.EMAIL_INPUT_FIELD_AUTH).send_keys('test_user_burgers123@ya.ru')
-        driver.find_element(*Locators.PASSWORD_INPUT_FIELD_AUTH).send_keys('123456')
-        driver.find_element(*Locators.SIGN_IN_BUTTON_AUTH).click()
-        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.PLACE_ORDER_BUTTON))
+        """Test authorization using the 'Sign In Account' button on the Main page."""
+        driver.find_element(*MainPageLocators.SIGN_IN_ACCOUNT_BUTTON_MAIN).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.EMAIL_INPUT_FIELD_AUTH))
+        driver.find_element(*AuthPageLocators.EMAIL_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_email)
+        driver.find_element(*AuthPageLocators.PASSWORD_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_password)
+        driver.find_element(*AuthPageLocators.SIGN_IN_BUTTON_AUTH).click()
+        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON))
 
-    # проверка входа через кнопку «Личный кабинет»
     def test_authorization_via_account_button(self, driver):
-        driver.find_element(*Locators.ACCOUNT_BUTTON_HEADER).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.EMAIL_INPUT_FIELD_AUTH))
-        driver.find_element(*Locators.EMAIL_INPUT_FIELD_AUTH).send_keys('test_user_burgers123@ya.ru')
-        driver.find_element(*Locators.PASSWORD_INPUT_FIELD_AUTH).send_keys('123456')
-        driver.find_element(*Locators.SIGN_IN_BUTTON_AUTH).click()
-        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.PLACE_ORDER_BUTTON))
+        """Test authorization using the 'Account' button."""
+        driver.find_element(*MainPageLocators.ACCOUNT_BUTTON_HEADER).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.EMAIL_INPUT_FIELD_AUTH))
+        driver.find_element(*AuthPageLocators.EMAIL_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_email)
+        driver.find_element(*AuthPageLocators.PASSWORD_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_password)
+        driver.find_element(*AuthPageLocators.SIGN_IN_BUTTON_AUTH).click()
+        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON))
 
-    # проверка входа через кнопку в форме регистрации
     def test_authorization_via_sign_in_register_button(self, driver):
-        driver.find_element(*Locators.ACCOUNT_BUTTON_HEADER).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.REGISTER_URL_AUTH)).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.SIGN_IN_BUTTON_REG)).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.EMAIL_INPUT_FIELD_AUTH))
-        driver.find_element(*Locators.EMAIL_INPUT_FIELD_AUTH).send_keys('test_user_burgers123@ya.ru')
-        driver.find_element(*Locators.PASSWORD_INPUT_FIELD_AUTH).send_keys('123456')
-        driver.find_element(*Locators.SIGN_IN_BUTTON_AUTH).click()
-        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.PLACE_ORDER_BUTTON))
+        """Test authorization using the 'Sign In' button on the registration form."""
+        driver.find_element(*MainPageLocators.ACCOUNT_BUTTON_HEADER).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.REGISTER_URL_AUTH)).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(RegisterPageLocators.SIGN_IN_BUTTON_REG)).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.EMAIL_INPUT_FIELD_AUTH))
+        driver.find_element(*AuthPageLocators.EMAIL_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_email)
+        driver.find_element(*AuthPageLocators.PASSWORD_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_password)
+        driver.find_element(*AuthPageLocators.SIGN_IN_BUTTON_AUTH).click()
+        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON))
 
-        # проверка входа через кнопку в форме восстановления пароля
     def test_authorization_via_sign_in_forgot_password_button(self, driver):
-        driver.find_element(*Locators.ACCOUNT_BUTTON_HEADER).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.FORGOT_PASSWORD_BUTTON_AUTH)).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.SIGN_IN_BUTTON_FORGOT_PASSWORD)).click()
-        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.EMAIL_INPUT_FIELD_AUTH))
-        driver.find_element(*Locators.EMAIL_INPUT_FIELD_AUTH).send_keys('test_user_burgers123@ya.ru')
-        driver.find_element(*Locators.PASSWORD_INPUT_FIELD_AUTH).send_keys('123456')
-        driver.find_element(*Locators.SIGN_IN_BUTTON_AUTH).click()
-        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(Locators.PLACE_ORDER_BUTTON))
+        """Test authorization using the 'Sign In' button on the forgot password form."""
+        driver.find_element(*MainPageLocators.ACCOUNT_BUTTON_HEADER).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.FORGOT_PASSWORD_BUTTON_AUTH)).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(ForgotPasswordPageLocators.SIGN_IN_BUTTON_FORGOT_PASSWORD)).click()
+        WebDriverWait(driver, 10).until(ec.visibility_of_element_located(AuthPageLocators.EMAIL_INPUT_FIELD_AUTH))
+        driver.find_element(*AuthPageLocators.EMAIL_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_email)
+        driver.find_element(*AuthPageLocators.PASSWORD_INPUT_FIELD_AUTH).send_keys(CommonData.test_user_password)
+        driver.find_element(*AuthPageLocators.SIGN_IN_BUTTON_AUTH).click()
+        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON))
